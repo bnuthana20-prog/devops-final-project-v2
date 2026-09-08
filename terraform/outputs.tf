1b4bfc44eedfbc24fd2ec33f1f6c564d7e51a5ad
bnuthana20-prog <bnuthana20@gmail.com>
2026-09-08 13:41:36 +0530

    Remove unused output variables from outputs.tf
    Removed unused output variables for VPC CIDR, instance type, and region.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,6 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "vpc_cidr" {
-  value = aws_vpc.main.cidr_block
-}
-
 output "public_subnet_id" {
   value = aws_subnet.public.id
 }
@@ -22,18 +18,10 @@ output "instance_id" {
   value = aws_instance.app.id
 }
 
-output "instance_type" {
-  value = aws_instance.app.instance_type
-}
-
 output "instance_public_ip" {
   value = aws_instance.app.public_ip
 }
 
 output "s3_bucket_name" {
   value = aws_s3_bucket.logs.bucket
-}
-
-output "region" {
-  value = var.aws_region
 }
