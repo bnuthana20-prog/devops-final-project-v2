c2bb919853e2a34129c84a99fe13492e3d5ada33
bnuthana20-prog <bnuthana20@gmail.com>
2026-09-08 13:28:01 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS Region Hyderabad"
+  type        = string
+  default     = "ap-south-2"
+}
+
+variable "ami_id" {
+  description = "AMI for ap-south-2"
+  type        = string
+  default     = "ami-0d0ad8bb301edb745"
+}
