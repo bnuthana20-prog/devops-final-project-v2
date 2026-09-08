29cc175b90a8c81f12659ed889b2e312ed8a463b
bnuthana20-prog <bnuthana20@gmail.com>
2026-09-08 13:29:23 +0530

    Add outputs for VPC, subnets, and instances

---
+++ terraform/outputs.tf
@@ -0,0 +1,39 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  value = aws_vpc.main.cidr_block
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.main.id
+}
+
+output "instance_id" {
+  value = aws_instance.app.id
+}
+
+output "instance_type" {
+  value = aws_instance.app.instance_type
+}
+
+output "instance_public_ip" {
+  value = aws_instance.app.public_ip
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.logs.bucket
+}
+
+output "region" {
+  value = var.aws_region
+}
