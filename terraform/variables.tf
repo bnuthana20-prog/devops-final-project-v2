30038fa60a2ebb63a7c2829a7b15ff8ab928f5d5
bnuthana20-prog <bnuthana20@gmail.com>
2026-09-08 14:23:02 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "aws_region" {
+  description = "AWS Region"
   type        = string
   default     = "ap-south-2"
 }
 
 variable "ami_id" {
+  description = "AMI ID for EC2"
   type        = string
-  default     = "ami-0a440ab90d09aa"
+  default     = "ami-0a440ab90d04d09aa"
 }
