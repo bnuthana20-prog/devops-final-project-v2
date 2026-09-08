d7b2a54952eaba7b3ac130a0ac1f2f8c9254b0fe
bnuthana20-prog <bnuthana20@gmail.com>
2026-09-08 13:34:40 +0530

    Clean up comment for AWS provider version
    Removed comment about expired key for AWS provider version.

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"   # FIXED: was 5.100.0 with expired key
+      version = "~> 5.0"  
     }
   }
 }
